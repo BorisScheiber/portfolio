0ed544a914f2739835422a4546d2dc58139c2a8c
Boris Scheiber <scheiberboris@hotmail.com>
2024-11-25 19:08:07 +0100

    privacy policy translation finished + fonts.sccs moved to styles folder

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,24 @@
 import { Component} from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+
+  restriction = {
+    cases: [
+      'dataProtection.generalInfo.restriction.cases.0',
+      'dataProtection.generalInfo.restriction.cases.1',
+      'dataProtection.generalInfo.restriction.cases.2',
+      'dataProtection.generalInfo.restriction.cases.3'
+    ]
+  };
+
 
   ngOnInit() {
     window.scrollTo({ top: 0, behavior: 'smooth' });

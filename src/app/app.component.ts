3a289c3180bde74fac9e136e926316a921899f64
Boris Scheiber <scheiberboris@hotmail.com>
2024-12-10 14:34:06 +0100

    aos implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { TranslateService } from '@ngx-translate/core';
 import { MobileNavOverlayComponent } from './shared/components/mobile-nav-overlay/mobile-nav-overlay.component';
-
+import AOS from 'aos';
 
 
 @Component({
@@ -21,6 +21,13 @@ export class AppComponent {
   constructor(private translate: TranslateService) {
     this.translate.onLangChange.subscribe((event) => {
       document.documentElement.lang = event.lang;
+    });
+  }
+
+  ngOnInit() {
+    AOS.init({
+      duration: 800,
+      once: false,         
     });
   }
 
